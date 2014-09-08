eb70b7ae6367936fe22756b5387d38b05361a98d
Benjamin Long <benjamin.long@fr.airtag.com>
2014-09-08 12:21:50 +0200

    Added topics in the home controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,10 @@
 var express = require('express'),
-  router = express.Router(),
-  Article = require('../models/article');
+  router = express.Router();
 
 var fs = require("fs");
 var Twitter = require('twitter-js-client').Twitter;
+var KeyWords = require('../models/keyWords');
+
 
 module.exports = function (app) {
   app.use('/', router);
@@ -11,18 +12,35 @@ module.exports = function (app) {
 
 router.get('/', function (req, res, next) {
 
+    var keyWords = new KeyWords();
+    keyWords.setFilterValue(10);
 
-  // var config = JSON.parse(fs.readFileSync('./config/twitter.js', encoding="ascii"));
-  // console.log(config)
+    var dataset_0 = JSON.parse(fs.readFileSync('./config/dataset_0.json', encoding="ascii"));
+    var dataset_1 = JSON.parse(fs.readFileSync('./config/dataset_1.json', encoding="ascii"));
+    var dataset_2 = JSON.parse(fs.readFileSync('./config/dataset_2.json', encoding="ascii"));
+    var dataset_3 = JSON.parse(fs.readFileSync('./config/dataset_3.json', encoding="ascii"));
 
-  var articles = [new Article(), new Article()];
+    var dataset = [];
+    dataset = dataset.concat(dataset_0);
+    dataset = dataset.concat(dataset_1);
+    dataset = dataset.concat(dataset_2);
+    dataset = dataset.concat(dataset_3);
+
+    keyWords.computeKeyWordsFromTweets(dataset);
+    var merged = keyWords.mergeKeyWords();
+    var topics = keyWords.createTopics(merged);
+
+    console.log("**************************************************");
+    console.log("topics : ");
+    console.log(topics);
+
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      title: 'daylines',
+      topics: topics
     });
 
 
-  // var twitter = new Twitter(config);
+    // var twitter = new Twitter(config);
 
   // console.log(twitter);
 
@@ -36,8 +54,5 @@ router.get('/', function (req, res, next) {
  //  twitter.getUserTimeline(
  //  	{ screen_name: 'Le_Figaro', count: '50'}, error, success);
 
-  var tweets = JSON.parse(fs.readFileSync('./config/dataset.json', encoding="ascii"));
-  
-  console.log(tweets);
-  
+
 });
